66662f6cf416286507d7ff56a7013de5f364f1c7
briandenicola <brian@briandenicola.com>
2024-05-08 14:03:30 -0500

    🏉 Wed May  8 14:03:30 CDT 2024

--- infrastructure/logging.tf
+++ infrastructure/logging.tf
@@ -2,22 +2,8 @@ resource "azurerm_log_analytics_workspace" "this" {
   name                          = local.azuremonitor_workspace_name
   location                      = azurerm_resource_group.this.location
   resource_group_name           = azurerm_resource_group.this.name
-  local_authentication_disabled = true
   sku                           = "PerGB2018"
   daily_quota_gb                = 10
-}
-
-resource "azurerm_log_analytics_solution" "this" {
-  solution_name         = "ContainerInsights"
-  location              = azurerm_resource_group.this.location
-  resource_group_name   = azurerm_resource_group.this.name
-  workspace_resource_id = azurerm_log_analytics_workspace.this.id
-  workspace_name        = azurerm_log_analytics_workspace.this.name
-
-  plan {
-    publisher = "Microsoft"
-    product   = "OMSGallery/ContainerInsights"
-  }
 }
 
 resource "azurerm_application_insights" "this" {
@@ -26,5 +12,4 @@ resource "azurerm_application_insights" "this" {
   resource_group_name           = azurerm_resource_group.this.name
   workspace_id                  = azurerm_log_analytics_workspace.this.id
   application_type              = "web"
-  local_authentication_disabled = false
 }
