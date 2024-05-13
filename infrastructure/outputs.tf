9bb71d9f48b7daa1ab067c6a44d8ff114a9dcbbd
briandenicola <brian@briandenicola.com>
2024-05-13 13:22:04 -0500

    Helper scripts

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -3,6 +3,11 @@ output "APP_NAME" {
   sensitive = false
 }
 
+output "ACR_NAME" {
+  value     = azurerm_container_registry.this.name
+  sensitive = false
+}
+
 output "AKS_CLUSTER_NAME" {
   value     = azurerm_kubernetes_cluster.this.name
   sensitive = false
---
+++ scripts/get-service-ipaddress.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# check if logged into azure exit if not
+az account show  >> /dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+  echo "Please login to Azure before updating firewall rules"
+  exit 1
+fi
+
+# check if resource group variable is defined else exit
+source ./setup-env.sh
+echo $RG | grep -i warning >> /dev/null 2>&1
+if [[ $? -eq 0 ]]; then
+    echo "Resource group not defined by terraform. Please create the environment by running 'task up'. Exiting."
+    exit 1
+fi
+
+export IP=`kubectl get svc aks-istio-ingressgateway-external -n aks-istio-ingress -o jsonpath='{.status.loadBalancer.ingress[0].ip}'`
+echo "Please create an DNS entry resolving ${IP}."
