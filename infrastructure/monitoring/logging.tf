390520565187188e96d6661acdbafba3aaee8fa6
briandenicola <brian@briandenicola.com>
2024-06-25 16:05:12 -0500

    Updated quota size

--- infrastructure/monitoring/logging.tf
+++ infrastructure/monitoring/logging.tf
@@ -3,7 +3,7 @@ resource "azurerm_log_analytics_workspace" "this" {
   location                      = azurerm_resource_group.monitoring.location
   resource_group_name           = azurerm_resource_group.monitoring.name
   sku                           = "PerGB2018"
-  daily_quota_gb                = 10
+  daily_quota_gb                = 1
 }
 
 resource "azurerm_application_insights" "this" {
