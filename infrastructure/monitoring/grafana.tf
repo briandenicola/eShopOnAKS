1848cdadc3bd305db4790fd65806856424481d45
briandenicola <brian@briandenicola.com>
2024-08-20 08:40:35 -0500

    🏐 Tue Aug 20 08:40:35 CDT 2024

--- infrastructure/monitoring/grafana.tf
+++ infrastructure/monitoring/grafana.tf
@@ -7,6 +7,7 @@ resource "azurerm_dashboard_grafana" "this" {
   resource_group_name               = azurerm_resource_group.monitoring.name
   location                          = var.core_region
   sku                               = "Standard"
+  grafana_major_version             = 10
   zone_redundancy_enabled           = false
   api_key_enabled                   = true
   deterministic_outbound_ip_enabled = true
