ef17cacb454976ddfe0eb9a80abc87794e214b94
briandenicola <brian@briandenicola.com>
2025-04-10 11:55:58 -0500

    🎱 Thu Apr 10 11:55:58 CDT 2025

--- infrastructure/aks/cluster-addons.tf
+++ infrastructure/aks/cluster-addons.tf
@@ -3,13 +3,14 @@ resource "azapi_update_resource" "cluster_updates" {
     azurerm_kubernetes_cluster.this
   ]
 
-  type        = "Microsoft.ContainerService/managedClusters@2024-03-02-preview"
+  type        = "Microsoft.ContainerService/managedClusters@2025-02-01"
   resource_id = azurerm_kubernetes_cluster.this.id
 
   body = jsonencode({
     properties = {
       networkProfile = {
         advancedNetworking = {
+          enabled = true
           observability = {
             enabled = true
           }
